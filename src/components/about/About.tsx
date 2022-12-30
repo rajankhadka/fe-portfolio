68b0da0a3d337baeb1e1b00d58056ef58714df6f
rajan.khadka <rajan.khadka@infodevelopers.com.np>
2022-12-30 17:37:28 +0545

    feat(about): adding about module

---
+++ src/components/about/About.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './about.css';
+import AboutImg from '../../assets/about.jpg';
+import CV from '../../assets/john-cv.pdf';
+function About() {
+  return (
+    <section className="about section" id="about">
+        <h2 className="section__title">About Me</h2>
+        <span className="section__subtitle">My introduction</span>
+
+        <div className="about__container container grid">
+
+        </div>
+    </section>
+  )
+}
+
+export default About;
