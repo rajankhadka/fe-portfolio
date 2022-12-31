c3c6a9d825aa898eee633a1a1115e9cf4e8c98ad
rajankhadka <rajankhadka.infodeveloper@gmail.com>
2023-01-01 01:05:24 +0545

    feat(module): implemented all module and alpha version completed

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
+import Qualification from "./components/qualification/Qualification";
+import Testimonial from "./components/testimonials/Testimonial";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import ScrollUp from "./components/ScrollUp/ScrollUp";
 
 function App() {
   return (
@@ -15,7 +20,12 @@ function App() {
         <About />
         <Skills />
         <Services />
+        <Qualification />
+        <Testimonial />
+        <Contact />
       </main>
+      <Footer />
+      <ScrollUp />
     </>
   );
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Rajan</h1>
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Portfolio
+            </a>
+          </li>
+
+          <li>
+            <a href="#testimonials" className="footer__link">
+              Testimonials
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://www.instagram.com/virus__mama/"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bx bxl-instagram"></i>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/rajan-khadka-33bb62b9/"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bx bxl-linkedin"></i>
+          </a>
+
+          <a
+            href="https://github.com/rajankhadka"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bx bxl-github"></i>
+          </a>
+        </div>
+
+        <span className="footer__copy">
+          &#169; Crypticalcoder. All rigths reserved
+        </span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
